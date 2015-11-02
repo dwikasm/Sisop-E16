916b0b6c4ea3791a785a78925c08fee6ff1e980e
dwikasm <dwikasm@gmail.com>
2015-11-02 15:51:11 +0700

    primanya udah dibenerin thread waitnya

--- prima.c
+++ prima.c
@@ -17,14 +17,16 @@ void *prime(void *args){
 }
 
 void main(){
-	pthread_t t1;
 	int input;
 	printf("Masukkan input : ");
 	scanf("%d",&input);
+	pthread_t t1[input];
 	int i;
 	for(i=2;i<input;i++){
-		pthread_create(&t1,NULL,prime,(void*)&i);
-		pthread_join(t1,NULL);
+		pthread_create(&t1[i-1],NULL,prime,(void*)&i);
+	}
+	for(i=2;i<input;i++){
+		pthread_join(t1[i-1],NULL);
 	}
 	printf("Jumlah bilangan prima sebelum %d adalah %d\n",input,jum);
 }
