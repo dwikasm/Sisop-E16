55917c5e80e85c91261b7c04ad42115690c3530c
nyarudea <adenuar11@gmail.com>
2015-11-05 20:46:13 +0700

    mengganti soal2.c , yang diganti adalah inisialisasi thread, penambahan for dan perubahan fungsi yang ada di thread

--- soal2.c
+++ soal2.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int a;
-char *token;
+int a,v=0;
+
 void *run(void *args)
 	{
-int v=0,d=a;
-while (d>0){if(d!=1)	{
+int *d=(int*)args;
+	if(*d!=1)	
+		{
 	int i=2;
 	int c=1;	
-	while(i<=d)	{
-		if(d%i==0)c++;		
+	while(i<=*d)	
+		{
+		if(*d%i==0)c++;		
 		i++;
-			}
+		}
 	if(c==2)v++;
-	}d--;}
-printf("jumlah bilangan prima kurang dari %d ada:%d\n",a,v);
+		}
 }
 
 void main()
 	{
-	pthread_t t1;
-	scanf("%d",&a);
-	pthread_create(&t1,NULL,run,NULL);	
-	pthread_join(t1,NULL);
-		
+	scanf("%d",&a);pthread_t t1[a];
+	int x,y,b[a];
+for(x=1;x<=a;x++){b[x]=x;pthread_create(&t1[x],NULL,run,(void*)&b[x]);	}
+for(y=1;y<=a;y++){	pthread_join(t1[y],NULL);}
+	printf("jumlah bilangan prima kurang dari %d ada:%d\n",a,v);	
 	}
