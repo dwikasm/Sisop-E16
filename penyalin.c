887376bd658eb3bf5f58fe378cbd0467625a47da
dwikasm <dwikasm@gmail.com>
2015-11-09 10:45:35 +0700

    penyalinnya sekarang udah pake input. Terus hasil salinannya jadi sesuai dengan nama file yang disalin

--- penyalin.c
+++ penyalin.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<stdlib.h>
-
+#include<string.h>
+char in[10001];
+pthread_t t1,t2;
 void *salin1(){
 	FILE *p1, *p2;
 	char temp;
-	p1=fopen("testkirim.txt","r");
-	p2=fopen("salinantestkirim.txt","w");
+	p1=fopen(in,"r");
+	char *token = strtok(in,".");
+	strcat(token,"-salinan.txt");
+	p2=fopen(token,"w");
 	while(1){
 		temp=fgetc(p1);
 		if(temp==EOF)break;
@@ -17,10 +21,13 @@ void *salin1(){
 	fclose(p2);
 }
 void *salin2(){
+	pthread_join(t1,NULL);
 	FILE *p1, *p2;
 	char temp;
-	p1=fopen("salinantestkirim.txt","r");
-	p2=fopen("salinannyasalinantestkirim.txt","w");
+	p1=fopen(in,"r");
+	char *token2 = strtok(in,".");
+	strcat(token2,"-salinan.txt");
+	p2=fopen(token2,"w");
 	while(1){
 		temp=fgetc(p1);
 		if(temp==EOF)break;
@@ -31,10 +38,10 @@ void *salin2(){
 	fclose(p2);
 }
 void main(){
-	pthread_t t1,t2;
+	printf("Masukkan nama file yang mau disalin : ");
+	scanf("%[^\n]",in);
 	pthread_create(&t1,NULL,salin1,NULL);
 	pthread_create(&t2,NULL,salin2,NULL);
-	pthread_join(t1,NULL);
 	pthread_join(t2,NULL);
 
 }
