4647c078bbee3d7fadae76e7c9fc624414e9f2de
dwikasm <dwikasm@gmail.com>
2015-11-09 16:51:51 +0700

    soal 2 udah dirapihin, terus algoritma pencarian primanya make yang lebih cepet. btw cara ngapus file dari git gimana ya wkwk

--- soal2.c
+++ soal2.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int a,v=0;
+int v=0;
 
-void *run(void *args)
-	{
-int *d=(int*)args;
-	if(*d!=1)	
-		{
+void *run(void *args){
+	int *d=(int*)args;
 	int i=2;
-	int c=1;	
-	while(i<=*d)	
-		{
-		if(*d%i==0)c++;		
+	while(i<=*d){
+		if(*d%i==0 && i==*d){
+			v++;
+			return;
+		}
+		else if(*d%i==0){
+			return;
+		}
 		i++;
-		}
-	if(c==2)v++;
-		}
+	}
 }
 
-void main()
-	{
-	scanf("%d",&a);pthread_t t1[a];
-	int x,y,b[a];
-for(x=1;x<=a;x++){b[x]=x;pthread_create(&t1[x],NULL,run,(void*)&b[x]);	}
-for(y=1;y<=a;y++){	pthread_join(t1[y],NULL);}
-	printf("jumlah bilangan prima kurang dari %d ada:%d\n",a,v);	
+void main(){
+	int a;
+	printf("Masukkan input : ");
+	scanf("%d",&a);
+	pthread_t t1[a];
+	int x,b[a];
+	for(x=2;x<a;x++){
+		b[x]=x;
+		pthread_create(&t1[x],NULL,run,(void*)&b[x]);
 	}
+	for(x=2;x<a;x++){
+		pthread_join(t1[x],NULL);
+	}
+	printf("jumlah bilangan prima kurang dari %d ada %d\n",a,v);	
+}
